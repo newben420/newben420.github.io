b5822457c24458e5149d2fd34760eb99dec7989c
newben420 <wraith@wraith.wraith>
2025-03-20 23:46:01 +0100

    added about section

---
+++ chunk-Z6PLOSQQ.js
@@ -0,0 +1,1 @@
+import{Ob as c,f as o,gb as n,xb as s,yb as p}from"./chunk-JPB2BJBV.js";o();var r=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=n({type:t,selectors:[["app-projects"]],decls:2,vars:0,template:function(e,i){e&1&&(s(0,"p"),c(1,"projects works!"),p())},encapsulation:2})};export{r as ProjectsComponent};
