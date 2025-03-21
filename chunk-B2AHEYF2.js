5ab46d8df41346743bd1fed4d50e7d76fe3546cc
newben420 <wraith@wraith.wraith>
2025-03-21 23:49:40 +0100

    working on skills

--- chunk-B2AHEYF2.js
+++
@@ -1,1 +0,0 @@
-import{Ab as s,Bb as p,Rb as c,f as o,ib as n}from"./chunk-LEZF76GM.js";o();var r=class t{static \u0275fac=function(e){return new(e||t)};static \u0275cmp=n({type:t,selectors:[["app-projects"]],decls:2,vars:0,template:function(e,i){e&1&&(s(0,"p"),c(1,"projects works!"),p())},encapsulation:2})};export{r as ProjectsComponent};
